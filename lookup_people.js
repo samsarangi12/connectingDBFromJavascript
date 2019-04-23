ea45358a2d8b5a188a3a1114df6abcbc671cff30
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-23 17:59:56 +0000

    Refactored the lookup_people script

--- lookup_people.js
+++ lookup_people.js
@@ -1,36 +1,55 @@
-const input = process.argv.slice(2)[0];
+function accpetInput () {
 
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
+  const input = process.argv.slice(2)[0];
+  return input;
+}
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
+function accessDetails () {
+  const input = accpetInput();
+  const pg = require("pg");
+  const settings = require("./settings"); // settings.json
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query(`SELECT first_name
-                      ,last_name
-                      ,birthdate
-                  from famous_people
-                 where first_name = $1`, [input], (err, result) => {
-  if (err) {
-    return console.error("error running query", err);
-  }
-  const queryResult = result.rows;
+  const client = new pg.Client({
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  });
+
+  client.connect((err) => {
+    if (err) {
+      return console.error("Connection Error", err);
+    }
+    client.query(`SELECT first_name
+                        ,last_name
+                        ,birthdate
+                    from famous_people
+                   where first_name = $1`, [input], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    displayOutput (result.rows);
+    client.end();
+    });
+  });
+}
+
+function displayOutput(item) {
+  const queryResult = item;
   for (let i = 0; i < queryResult.length; i++) {
     let firstName = queryResult[i].first_name;
     let lastName = queryResult[i].last_name;
     let dateOfBirth = queryResult[i].birthdate.toISOString().slice(0,10);
     console.log(`- ${i+1} ${firstName} ${lastName}, born '${dateOfBirth}'`);
   }
-  client.end();
-  });
-});
+}
+
+accessDetails ();
+
+
+
+
+
+
